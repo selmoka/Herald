dfaa779d453120bc58f769600fe5d04a3f3ad66b
Selmo <45966506+selmoka@users.noreply.github.com>
2019-04-07 11:49:15 +0300

    Changed models

--- herald/models.py
+++ herald/models.py
@@ -5,9 +5,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Author(models.Model):
 	name = models.CharField(max_length=50)
+	user = models.OneToOneField(User, on_delete=models.CASCADE,default=2)
 	def __str__(self):
 		return self.name
 
+class Visitor(models.Model):
+	name = models.CharField(max_length=50)
+	user = models.OneToOneField(User, on_delete=models.CASCADE,default=None)
 # class User(models.Model):
 # 	name = models.CharField(max_length=50)
 # 	def __str__(self):
@@ -25,8 +29,10 @@ class Article(models.Model):
 
 class Comment(models.Model):
 	comment_text = models.TextField(blank=True,null=True)
+	pub_date = models.DateTimeField(blank=True,null=True)
 	# user_comment = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-	user_comment = 'a1'
+	visitor_comment = models.ForeignKey(Visitor, on_delete=models.CASCADE, null=True, 
+		blank=True, default=None)
 	article_comment = models.ForeignKey(Article, on_delete=models.CASCADE, null=True)
 	def __str__(self):
 		return self.user_comment
