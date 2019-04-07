b337c64c9384f569853dc38bfd2eadb1b567a2e3
Selmo <45966506+selmoka@users.noreply.github.com>
2019-04-07 10:06:56 +0300

    Fixed identation

--- fnews/urls.py
+++ fnews/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('', include('herald.urls'), name='herald'),
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
 
 ]
